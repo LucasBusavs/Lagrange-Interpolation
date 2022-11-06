0f22a92a7b6db02ecffc80c1c6120bfee5572dfe
Lucas Bueno <turmadaciencia1@gmail.com>
2022-11-06 00:10:22 -0300

    Working

--- LagrangeInterpolation.c
+++ LagrangeInterpolation.c
@@ -5,6 +5,7 @@
 int main(){
 
     int n;
+    float xp, p, fp = 0;
 
     printf("Enter amount of data: ");
     scanf("%d", &n);
@@ -12,14 +13,27 @@ int main(){
     float x[n];
     float fx[n];
 
-    printf("Enter data:\n");
     for (int i = 0; i<n; i++){
-        printf("Enter with x[%d]", i);
+        printf("Enter with x[%d] = ", i);
         scanf("%f", &x[i]);
-        printf("Enter with fx[%d]", i);
+        printf("Enter with fx[%d] = ", i);
         scanf("%f", &fx[i]);
     }
 
+    printf("Enter interpolation point: ");
+	scanf("%f", &xp);
+
+    for(int i = 0; i<n; i++){
+        p = 1;
+        for(int j = 0; j<n; j++){
+            if(i!=j){
+                p =  p * (xp - x[j])/(x[i]-x[j]);
+            }
+        }
+        fp = fp + p * fx[i];
+    }
+
+    printf("Interpolated value at %.4f is %.4f.", xp, fp);
 
     return 0;
 }
